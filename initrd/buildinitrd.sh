c615e2e0791a5a7959de505018b33a175852125c
Eric B Munson <munsoner@bu.edu>
2025-08-25 15:41:28 +0000

    Work in progress, need to update shared machine to deb13
    Signed-off-by: Eric B Munson <munsoner@bu.edu>

--- initrd/buildinitrd.sh
+++ initrd/buildinitrd.sh
@@ -7,30 +7,31 @@ copy()
   local file
 
   if [ "$2" = "lib" ]; then
-    file=$(PATH=/usr/lib:/usr/lib64 type -p $1)
+    file=$(PATH=/usr/lib:/usr/lib64:/usr/lib/x86_64-linux-gnu type -p $1)
   else
     file=$(type -p $1)
   fi
 
   if [ -n "$file" ] ; then
-    cp $file $WDIR/$file
+    cp $file ${WDIR}$file
   else
     echo "Missing required file: $1 for directory $2"
-    rm -rf $WDIR
+    rm -rf ${WDIR}
     exit 1
   fi
 }
 
 printf "Creating initramfs structure ... "
 
-cp ./perf usr/sbin/
+set -xe
+cp ./perf /usr/sbin/
 
-binfiles="cat ls mkdir mknod mount bash top touch awk less"
+binfiles="cat ls mkdir lspci mknod mount bash top touch awk less"
 binfiles="$binfiles umount sed sleep ln rm uname grep nproc"
 binfiles="$binfiles readlink basename chmod ps pidof pgrep pkill"
 binfiles="$binfiles cut netstat"
 
-sbinfiles="halt dropbear ip rdmsr wrmsr lspci perf ethtool"
+sbinfiles="halt dropbear ip rdmsr wrmsr perf ethtool"
 
 unsorted=$(mktemp /tmp/unsorted.XXXXXXXXXX)
 
@@ -39,38 +40,44 @@ INITIN=./init
 # Create a temporary working directory
 WDIR=$1
 
+mkdir -p ${WDIR}
+chmod 775 ${WDIR}
+
 # Create base directory structure
-mkdir -p $WDIR/{dev,etc/dropbear,run,sys,proc,usr/{bin,lib,lib64,sbin},var/run}
-ln -s usr/bin  $WDIR/bin
-ln -s usr/lib  $WDIR/lib
-ln -s usr/sbin $WDIR/sbin
-ln -s lib      $WDIR/lib64
+for dir in  "${WDIR}/dev" "${WDIR}/etc/dropbear" "${WDIR}/run" "${WDIR}/sys" "${WDIR}/proc" "${WDIR}/usr/bin" "${WDIR}/usr/lib/x86_64-linux-gnu" "${WDIR}/usr/lib64" "${WDIR}/usr/sbin" "${WDIR}/var/run" 
+do
+	mkdir -p $dir
+done
+ln -s usr/bin  ${WDIR}/bin
+ln -s usr/lib  ${WDIR}/lib
+ln -s usr/sbin ${WDIR}/sbin
+ln -s lib      ${WDIR}/lib64
 
 # Create necessary device nodes
-mknod -m 640 $WDIR/dev/tty0    c 4 0
-mknod -m 640 $WDIR/dev/tty1    c 4 1
-mknod -m 640 $WDIR/dev/tty     c 5 0
-mknod -m 640 $WDIR/dev/console c 5 1
-mknod -m 644 $WDIR/dev/ptmx    c 5 2
-mknod -m 664 $WDIR/dev/null    c 1 3
-mknod -m 664 $WDIR/dev/zero    c 1 5
-mknod -m 664 $WDIR/dev/random  c 1 8
-mknod -m 664 $WDIR/dev/urandom c 1 9
-mknod -m 664 $WDIR/dev/loop0   b 7 0
-mknod -m 664 $WDIR/dev/loop1   b 7 1
-mkdir -m 755 $WDIR/dev/pts
-mknod -m 600 $WDIR/dev/pts/0   c 136 0
-mknod -m 000 $WDIR/dev/pts/ptmx c 5 2
+mknod -m 640 ${WDIR}/dev/tty0    c 4 0
+mknod -m 640 ${WDIR}/dev/tty1    c 4 1
+mknod -m 640 ${WDIR}/dev/tty     c 5 0
+mknod -m 640 ${WDIR}/dev/console c 5 1
+mknod -m 644 ${WDIR}/dev/ptmx    c 5 2
+mknod -m 664 ${WDIR}/dev/null    c 1 3
+mknod -m 664 ${WDIR}/dev/zero    c 1 5
+mknod -m 664 ${WDIR}/dev/random  c 1 8
+mknod -m 664 ${WDIR}/dev/urandom c 1 9
+mknod -m 664 ${WDIR}/dev/loop0   b 7 0
+mknod -m 664 ${WDIR}/dev/loop1   b 7 1
+mkdir -m 755 ${WDIR}/dev/pts
+mknod -m 600 ${WDIR}/dev/pts/0   c 136 0
+mknod -m 000 ${WDIR}/dev/pts/ptmx c 5 2
 
 
 # Install the init file
-install -m0755 $INITIN $WDIR/init
+install -m0755 $INITIN ${WDIR}/init
 
 # At somepoint dropbear stopped supporting scp directly and now requires access
 # to an sftp server so we have to pull in enough of openssh to make that work
-mkdir -p $WDIR/usr/libexec/openssh/
-ldd /usr/libexec/openssh/sftp-server | sed "s/\t//" | cut -d " " -f1 >> $unsorted
-copy /usr/libexec/openssh/sftp-server usr/libexec/openssh/
+mkdir -p ${WDIR}/usr/lib/openssh/
+ldd /usr/lib/openssh/sftp-server | sed "s/\t//" | cut -d " " -f1 >> $unsorted
+copy /usr/lib/openssh/sftp-server usr/lib/openssh/
 
 # Install basic binaries
 for f in $binfiles ; do
@@ -78,7 +85,7 @@ for f in $binfiles ; do
   copy /usr/bin/$f bin
 done
 
-ln -s bash $WDIR/usr/bin/sh
+ln -s bash ${WDIR}/usr/bin/sh
 
 for f in $sbinfiles ; do
   ldd /usr/sbin/$f | sed "s/\t//" | cut -d " " -f1 >> $unsorted
@@ -99,18 +106,18 @@ sort $unsorted | uniq | while read library ; do
   copy $library lib
 done
 
-mkdir -p $WDIR/usr/share
+mkdir -p ${WDIR}/usr/share
 
-cp -r /usr/share/terminfo $WDIR/usr/share
+cp -r /usr/share/terminfo ${WDIR}/usr/share
 
-conf="bashrc bash_completion.d group hosts passwd profile"
-conf="$conf shells shadow sysconfig"
+conf="bash.bashrc bash_completion.d group hosts passwd profile"
+conf="$conf shells shadow"
 for f in $conf ; do
-  cp -r /etc/$f $WDIR/etc/$f
+  cp -r /etc/$f ${WDIR}/etc/$f
 done
 
 if [ -z "$2" -a -s "$2" ] ; then
-	cp $2 $WDIR/usr/bin/
+	cp $2 ${WDIR}/usr/bin/
 fi
 
 if [ -s data.tar.gz ] ; then
@@ -118,11 +125,8 @@ if [ -s data.tar.gz ] ; then
 fi
 
 cp -r exp ${WDIR}
+mv lib/modules ${WDIR}/lib
 
 rm -f $unsorted
-
-pushd $WDIR
-find . | cpio -o -H newc | xz --format=lzma > ../ukl-initrd.cpio.xz
-popd
 
 printf "done.\n"
